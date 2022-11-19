80f56254532f4ef0c5feb7dbe2845bd088a2c51e
Imamura Shohei <s3imamura@gmail.com>
2022-11-19 22:25:15 +0900

    移動

--- src/components/MyCanvas.vue
+++ src/components/MyCanvas.vue
@@ -1,64 +1,95 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, watch } from "vue";
-import { planet } from './Planet.vue'
-import type { Planet } from './Planet.vue'
+import { planet } from "./Planet.vue";
+import type { Planet, Moon } from "./Planet.vue";
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
 
 const ctx = ref<CanvasRenderingContext2D | null>(null);
+const canvasScale = ref(200);
+const centerCoordinate = ref<Coordinate>({
+  x: canvasScale.value,
+  y: canvasScale.value,
+});
 
 // 惑星
-const { Mercury, Venus, Earth, Jupiter, Saturn } = planet();
+const { Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, TheMoon } =
+  planet();
+
+// 時刻
+const monthCount = ref(0);
+const incrementMonth = () => {
+  monthCount.value++;
+};
+const timeScale = ref(1 / 360);
 
 onMounted(() => {
-  console.log("on mounted...");
   // canvas要素を取得
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   ctx.value = canvas.getContext("2d");
   if (ctx.value === null) return;
+  drawSolarSystem();
+  setInterval(incrementMonth, 50); // todo 例示のために表示 後で消す
+});
 
-  ctx.value.clearRect(0, 0, 400, 400);
+watch(monthCount, () => {
+  // 変更が発生するごとに再描画
+  drawSolarSystem();
+});
+
+const drawSolarSystem = () => {
+  if (ctx.value === null) return;
+
+  ctx.value.clearRect(0, 0, canvasScale.value * 2, canvasScale.value * 2);
 
   // 背景
-  drawBackGround(200);
+  drawBackGround(canvasScale.value);
+
+  // 天王星
+  drawPlanetaryOrbit(Uranus);
+  drawPlanetaryArcArea(Uranus, monthCount.value, 1);
+  drawPlanet(Uranus, monthCount.value);
+  drawBackGround(Uranus.radius - 10); // デザイン調整
+  // 土星
+  drawPlanetaryOrbit(Saturn);
+  drawPlanetaryArcArea(Saturn, monthCount.value, 1);
+  drawPlanet(Saturn, monthCount.value);
+  drawBackGround(Saturn.radius - 10); // デザイン調整
+  // 木星
+  drawPlanetaryOrbit(Jupiter);
+  drawPlanetaryArcArea(Jupiter, monthCount.value, 1);
+  drawPlanet(Jupiter, monthCount.value);
+  drawBackGround(Jupiter.radius - 18); // デザイン調整
+  // 火星
+  drawPlanetaryOrbit(Mars);
+  drawPlanetaryArcArea(Mars, monthCount.value, 1 / 12);
+  drawPlanet(Mars, monthCount.value);
+  // 地球
+  drawPlanetaryOrbit(Earth);
+  drawPlanetaryArcArea(Earth, monthCount.value, 1 / 12);
+  drawPlanet(Earth, monthCount.value);
+  // 金星
+  drawPlanetaryOrbit(Venus);
+  drawPlanetaryArcArea(Venus, monthCount.value, 1 / 12);
+  drawPlanet(Venus, monthCount.value);
 
   // 水星
   drawPlanetaryOrbit(Mercury);
-  drawPlanetaryArcArea(Mercury, 4, 1);
-  drawPlanet(Mercury, 4);
-
-  // 金星
-  drawPlanetaryOrbit(Venus);
-  drawPlanetaryArcArea(Venus, 1, 1);
-  drawPlanet(Venus, 1);
-
-  // 地球
-  drawPlanetaryOrbit(Earth);
-  drawPlanetaryArcArea(Earth, 23, 1);
-  drawPlanet(Earth, 23);
-
-
-  // 木星
-  drawPlanetaryOrbit(Jupiter);
-  drawPlanetaryArcArea(Jupiter, 15, 1);
-  drawPlanet(Jupiter, 15);
-
-  // 土星
-  drawPlanetaryOrbit(Saturn);
-  drawPlanetaryArcArea(Saturn, 25, 1);
-  drawPlanet(Saturn, 25);
+  drawPlanetaryArcArea(Mercury, monthCount.value, 1 / 12);
+  drawPlanet(Mercury, monthCount.value);
 
   // 太陽（恒星）
   drawStar(3);
 
   // 月（衛星）
-  drawSatellite(Earth.radius, Earth.angularVelocity * 23);
-  drawSatelliteCircleLine(
-    Earth.radius,
-    Earth.angularVelocity * 23,
-    20
-  );
-
-});
+  drawMoonOrbit(TheMoon, monthCount.value);
+  drawMoonArcArea(TheMoon, monthCount.value, 1 / 48);
+  drawMoon(TheMoon, monthCount.value);
+};
 
 // 背景
 const drawBackGround = (radius: number) => {
@@ -70,22 +101,6 @@ const drawStar = (radius: number) => {
   drawFilledCircle(radius, "rgb(90,20,30)"); // 太陽の色
 };
 
-// 衛星を描画
-const drawSatellite = (
-  radius: number,
-  radians: number = 0,
-  fillColor: string = "rgba(255,255,200,0.9)"
-) => {
-  if (ctx.value === null) return;
-  ctx.value.beginPath();
-  // 位置を導出
-  const { x, y } = getXYByRadians(radians, radius);
-  ctx.value.arc(x, y, 3, 0, Math.PI * 2);
-
-  ctx.value.fillStyle = fillColor;
-  ctx.value.fill();
-};
-
 // 惑星を描画
 const drawPlanet = (
   planet: Planet,
@@ -95,17 +110,26 @@ const drawPlanet = (
   if (ctx.value === null) return;
   ctx.value.beginPath();
   // 位置を導出
-  const { x, y } = getXYByRadians(planet.angularVelocity * time, planet.radius);
-  ctx.value.arc(x, y, 3, 0, Math.PI * 2);
+  const { x, y } = getXYByRadians(
+    planet.angularVelocity * time * timeScale.value,
+    planet.radius
+  );
+  ctx.value.arc(
+    centerCoordinate.value.x + x,
+    centerCoordinate.value.y + y,
+    3,
+    0,
+    Math.PI * 2
+  );
 
   ctx.value.fillStyle = fillColor;
   ctx.value.fill();
 };
 
 // 位置を導出
-const getXYByRadians = (radians: number, radius: number) => {
-  const y = 200 + Math.sin(radians) * radius;
-  const x = 200 + Math.cos(radians) * radius;
+const getXYByRadians = (radians: number, radius: number): Coordinate => {
+  const x = Math.cos(radians) * radius;
+  const y = -Math.sin(radians) * radius;
   return {
     x: x,
     y: y,
@@ -116,37 +140,32 @@ const getXYByRadians = (radians: number, radius: number) => {
 const drawFilledCircle = (radius: number, fillColor: string = "black") => {
   if (ctx.value === null) return;
   ctx.value.beginPath();
-  ctx.value.arc(200, 200, radius, 0, Math.PI * 2);
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    radius,
+    0,
+    Math.PI * 2
+  );
   ctx.value.fillStyle = fillColor;
   ctx.value.fill();
 };
 // 惑星軌道を描画
 const drawPlanetaryOrbit = (
   planet: Planet,
-  lineColor: string = "rgba(99,99,99, 0.6)" // 軌道の色
+  lineColor: string = "rgba(130,130,130,0.6)" // 軌道の色
 ) => {
   if (ctx.value === null) return;
   ctx.value.beginPath();
   ctx.value.strokeStyle = lineColor;
-  ctx.value.arc(200, 200, planet.radius, 0, 2 * Math.PI);
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    planet.radius,
+    0,
+    2 * Math.PI
+  );
   ctx.value.stroke();
-};
-
-// 惑星円弧塗り潰し
-const drawFilledArc = (
-  radius: number,
-  radiansStart: number = 0, // 開始角度
-  radians: number = Math.PI * (1 / 4), // 角度
-  fillColor: string = "rgba(99,99,99, 0.5)" // 扇形の色
-) => {
-  if (ctx.value === null) return;
-  ctx.value.beginPath();
-
-  ctx.value.moveTo(200, 200);
-  ctx.value.arc(200, 200, radius, radiansStart, radiansStart + radians);
-  ctx.value.fillStyle = fillColor;
-  ctx.value.closePath();
-  ctx.value.fill();
 };
 
 // 惑星円弧塗り潰し
@@ -154,49 +173,110 @@ const drawPlanetaryArcArea = (
   planet: Planet,
   time: number = 0, // 開始角度
   period: number = 1, // 角度
-  fillColor: string = "rgba(99,99,99, 0.75)" // 扇形の色
+  fillColor: string = "rgba(255,255,255, 0.5)" // 扇形の色
 ) => {
   if (ctx.value === null) return;
   ctx.value.beginPath();
 
-  ctx.value.moveTo(200, 200);
-  ctx.value.arc(200, 200, planet.radius, planet.angularVelocity * time, planet.angularVelocity * (time + period));
+  ctx.value.moveTo(centerCoordinate.value.x, centerCoordinate.value.y);
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    planet.radius,
+    planet.angularVelocity * time * -1 * timeScale.value,
+    planet.angularVelocity * (time * -1 * timeScale.value + period)
+  );
   ctx.value.fillStyle = fillColor;
   ctx.value.closePath();
   ctx.value.fill();
 };
 
 // 衛星軌道
-const drawSatelliteCircleLine = (
-  planetRadius: number,
-  planetRadians: number,
-  satelliteRadius: number,
+const drawMoonOrbit = (
+  moon: Moon,
+  time: number = 0,
   lineColor: string = "rgba(99,99,99, 0.4)" // 軌道の色
 ) => {
   if (ctx.value === null) return;
   // 惑星の位置を導出
-  const { x, y } = getXYByRadians(planetRadians, planetRadius);
+  const { x, y } = getXYByRadians(
+    moon.planet.angularVelocity * time * timeScale.value,
+    moon.planet.radius
+  );
   ctx.value.beginPath();
   ctx.value.strokeStyle = lineColor;
-  ctx.value.arc(x, y, satelliteRadius, 0, 2 * Math.PI);
+  ctx.value.arc(
+    centerCoordinate.value.x + x,
+    centerCoordinate.value.y + y,
+    moon.radius,
+    0,
+    2 * Math.PI,
+    true
+  );
   ctx.value.stroke();
 };
 
-// 衛星描画
-const drawSatellitePointCircle = (
-  planetRadius: number,
-  planetRadians: number,
-  satelliteRadius: number,
-  satelliteRadians: number,
-  lineColor: string = "rgba(99,99,99, 0.4)" // 軌道の色
+// 衛星を描画
+const drawMoon = (
+  moon: Moon,
+  time: number = 0,
+  fillColor: string = "rgba(255,255,200,0.9)"
 ) => {
   if (ctx.value === null) return;
   // 惑星の位置を導出
-  const { x, y } = getXYByRadians(planetRadians, planetRadius);
+  const { x: planetX, y: planetY } = getXYByRadians(
+    moon.planet.angularVelocity * time * timeScale.value,
+    moon.planet.radius
+  );
   ctx.value.beginPath();
-  ctx.value.strokeStyle = lineColor;
-  ctx.value.arc(x, y, satelliteRadius, 0, 2 * Math.PI);
-  ctx.value.stroke();
+  // 位置を導出
+  const { x, y } = getXYByRadians(
+    moon.angularVelocity * time * timeScale.value,
+    moon.radius
+  );
+  ctx.value.arc(
+    centerCoordinate.value.x + planetX + x,
+    centerCoordinate.value.y + planetY + y,
+    2,
+    0,
+    Math.PI * 2
+  );
+
+  ctx.value.fillStyle = fillColor;
+  ctx.value.fill();
+};
+
+// 衛星円弧塗り潰し
+const drawMoonArcArea = (
+  moon: Moon,
+  time: number = 0, // 開始角度
+  period: number = 1, // 角度
+  fillColor: string = "rgba(255,255,255, 0.5)" // 扇形の色
+) => {
+  if (ctx.value === null) return;
+  // 惑星の位置を導出
+  const { x: planetX, y: planetY } = getXYByRadians(
+    moon.planet.angularVelocity * time * timeScale.value,
+    moon.planet.radius
+  );
+  ctx.value.beginPath();
+
+  // 惑星位置まで移動
+  ctx.value.moveTo(
+    centerCoordinate.value.x + planetX,
+    centerCoordinate.value.y + planetY
+  );
+  ctx.value.arc(
+    centerCoordinate.value.x + planetX,
+    centerCoordinate.value.y + planetY,
+    moon.radius,
+    moon.angularVelocity * time * -1 * timeScale.value,
+    moon.angularVelocity * (time * -1 * timeScale.value + period)
+  );
+
+  ctx.value.fillStyle = fillColor;
+  ctx.value.closePath();
+  ctx.value.fill();
 };
 </script>
 
@@ -205,6 +285,8 @@ const drawSatellitePointCircle = (
   border: 1px solid #000;
 }
 </style>
+
 <template>
-  <canvas width="400" height="400" class="canvas" id="canvas"> </canvas>
+  <canvas :width="canvasScale * 2" :height="canvasScale * 2" class="canvas" id="canvas">
+  </canvas>
 </template>
--- src/components/Planet.vue
+++ src/components/Planet.vue
@@ -1,43 +1,91 @@
 <script lang="ts">
 export type Planet = {
-  radius: number,
-  angularVelocity: number,
-}
+  radius: number; // 半径（便宜的に長半径を指定）
+  orbitalPeriod: number; // 公転周期
+  angularVelocity: number;
+};
+export type Moon = {
+  planet: Planet; // 惑星
+  radius: number; // 半径（便宜的に長半径を指定）
+  orbitalPeriod: number; // 公転周期
+  angularVelocity: number;
+};
+
 export const planet = () => {
   // 水星
   const Mercury: Planet = {
-    radius: 60,
-    angularVelocity: (Math.PI * (6 / 24)),
+    radius: 39,
+    orbitalPeriod: 0.24,
+    angularVelocity: Math.PI * 2 * (1 / 0.24),
   };
 
   // 金星
   const Venus: Planet = {
-    radius: 80,
-    angularVelocity: (Math.PI * (4 / 24)),
+    radius: 72,
+    orbitalPeriod: 0.62,
+    angularVelocity: Math.PI * 2 * (1 / 0.62),
   };
 
   // 地球
   const Earth: Planet = {
-    radius: 120,
-    angularVelocity: (Math.PI * (2 / 24)),
+    radius: 100,
+    orbitalPeriod: 1,
+    angularVelocity: Math.PI * 2 * (1 / 1),
+  };
+
+  // 火星
+  const Mars: Planet = {
+    radius: 152,
+    orbitalPeriod: 1.88,
+    angularVelocity: Math.PI * 2 * (1 / 1.88),
   };
 
   // 木星
   const Jupiter: Planet = {
-    radius: 150,
-    angularVelocity: (Math.PI * (1 / 24)),
+    radius: 170,
+    orbitalPeriod: 11.8,
+    angularVelocity: Math.PI * 2 * (1 / 11.8),
   };
-
   // 土星
   const Saturn: Planet = {
     radius: 180,
-    angularVelocity: (Math.PI * (1 / 24)),
+    orbitalPeriod: 29.4,
+    angularVelocity: Math.PI * 2 * (1 / 29.4),
+  };
+  // 天王星
+  const Uranus: Planet = {
+    radius: 190,
+    orbitalPeriod: 84.0,
+    angularVelocity: Math.PI * 2 * (1 / 84.0),
+  };
+  // 海王星
+  const Neptune: Planet = {
+    radius: 200,
+    orbitalPeriod: 164,
+    angularVelocity: Math.PI * 2 * (1 / 164),
+  };
+
+  // 月
+  const TheMoon: Moon = {
+    planet: Earth,
+    radius: 15,
+    orbitalPeriod: 0.075,
+    angularVelocity: Math.PI * 2 * (1 / 0.075),
   };
   return {
-    Mercury, Venus, Earth, Jupiter, Saturn
-  }
-}
-export default {
+    // 惑星（Planet）
+    Mercury,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune,
+    // 衛星(Moon)
+    TheMoon,
+  };
+};
 
-}
+export default {};
 </script>
