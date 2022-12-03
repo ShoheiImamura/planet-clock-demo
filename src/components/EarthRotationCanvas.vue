ed05c48220efec6d5bb2adc811d77523a5fba375
Imamura Shohei <s3imamura@gmail.com>
2022-12-03 14:36:09 +0900

    unix time を利用

---
+++ src/components/EarthRotationCanvas.vue
@@ -0,0 +1,294 @@
+
+<script lang="ts" setup>
+import { ref, onMounted, watch } from "vue";
+import { planet } from "./Planet.vue";
+import { Planet, Moon, } from "./Planet.vue";
+import { planetaryConjunction } from "./PlanetaryConjunction.vue";
+import type { planetaryConjunctionType } from "./PlanetaryConjunction.vue";
+
+const props = defineProps({
+  dayCount: {
+    type: Number,
+    default: 0,
+  }
+});
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
+const ctx = ref<CanvasRenderingContext2D | null>(null);
+const canvasScale = ref(150);
+const centerCoordinate = ref<Coordinate>({
+  x: canvasScale.value,
+  y: canvasScale.value,
+});
+
+// 惑星
+const { Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, TheMoon } = planet();
+// 惑星会合
+const { isInnerConjunction, isOuterConjunction } = planetaryConjunction();
+
+onMounted(() => {
+  // canvas要素を取得
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  ctx.value = canvas.getContext("2d");
+  if (ctx.value === null) return;
+  drawSolarSystem();
+  setInterval(drawSolarSystem, 10);
+});
+
+/** 日数から年数への変換 */
+const dayToYear = (day: number): number => {
+  return day / 365.25;
+}
+
+// 位置を導出
+const getXYByRadians = (radians: number, radius: number): Coordinate => {
+  const x = Math.cos(radians) * radius;
+  const y = -Math.sin(radians) * radius;
+  return {
+    x: x,
+    y: y,
+  };
+};
+const planetList = [Uranus, Saturn, Jupiter, Mars, Earth, Venus, Mercury];
+
+// 惑星会合
+const eventPlanetaryConjunction = (year: number) => {
+  const conjunctionList: planetaryConjunctionType[] = [];
+  planetList.forEach((planet1, index1) => {
+    planetList.forEach((planet2, index2) => {
+      if (index1 <= index2) {
+        return;
+      }
+      if (isInnerConjunction(planet1, planet2, year)) {
+        conjunctionList.push({ planet1: planet1, planet2: planet2, conjunctionType: 'inner' });
+      }
+      if (isOuterConjunction(planet1, planet2, year)) {
+        conjunctionList.push({ planet1: planet1, planet2: planet2, conjunctionType: 'outer' });
+      }
+    });
+  })
+  return conjunctionList;
+}
+
+const drawSolarSystem = () => {
+  if (ctx.value === null) return;
+
+  ctx.value.clearRect(0, 0, canvasScale.value * 2, canvasScale.value * 2);
+
+  // 背景
+  drawBackGround(canvasScale.value);
+
+  // 惑星
+  const planetaryConjunctionList = eventPlanetaryConjunction(dayToYear(props.dayCount))
+  if (planetaryConjunctionList.length !== 0) {
+    // 惑星会合ありの場合
+    planetList.forEach(planet => {
+      // list に planet 自身が含まれるかどうか
+      const isConjanction = planetaryConjunctionList.some((planetaryConjunction) => {
+        return planet == planetaryConjunction.planet1 || planet == planetaryConjunction.planet2
+      });
+      if (isConjanction) {
+        drawPlanetaryOrbit(planet);
+        drawLineStarToPlanet(planet, props.dayCount, 'rgba(255,255,255,1)', 2);
+        drawPlanet(planet, props.dayCount, planet.size);
+      } else {
+        drawPlanetaryOrbit(planet);
+        drawLineStarToPlanet(planet, props.dayCount, 'rgba(120,120,120,0.5)',);
+        drawPlanet(planet, props.dayCount, planet.size, 'rgba(120,120,120,0.5)');
+      }
+    });
+  } else {
+    // 惑星会合なしの場合
+    planetList.forEach(planet => {
+      drawPlanetaryOrbit(planet);
+      drawLineStarToPlanet(planet, props.dayCount);
+      drawPlanet(planet, props.dayCount, planet.size);
+    });
+  }
+
+  // 太陽（恒星）
+  drawStar(3);
+
+  // 月（衛星）
+  drawMoonOrbit(TheMoon, props.dayCount);
+  drawMoon(TheMoon, props.dayCount);
+};
+
+// 背景
+const drawBackGround = (radius: number) => {
+  const forCount = 2
+  for (let index = 0; index < forCount; index++) {
+    drawFilledCircle(radius, "black", Math.PI * (index * 2 + 0) / forCount, Math.PI * (index * 2 + 1) / forCount);
+    drawFilledCircle(radius, "rgb(20,20,20)", Math.PI * (index * 2 + 1) / forCount, Math.PI * (index * 2 + 2) / forCount);
+  }
+};
+
+// 恒星を描画
+const drawStar = (radius: number) => {
+  drawFilledCircle(radius, "rgba(255,255,200,0.9)"); // 太陽の色
+};
+
+// 惑星を描画
+const drawPlanet = (
+  planet: Planet,
+  days: number = 0, // 基準日からの日数
+  planetRadius: number = 3,
+  fillColor: string = "rgba(255,255,200,0.9)",
+) => {
+  if (ctx.value === null) return;
+  ctx.value.beginPath();
+  // 位置を導出
+  const { x, y } = getXYByRadians(
+    planet.angle(dayToYear(days)),
+    planet.radiusRatio * canvasScale.value / 2
+  );
+  ctx.value.arc(
+    centerCoordinate.value.x + x,
+    centerCoordinate.value.y + y,
+    planetRadius,
+    0,
+    Math.PI * 2
+  );
+
+  ctx.value.fillStyle = fillColor;
+  ctx.value.fill();
+};
+
+
+// 丸塗り潰し
+const drawFilledCircle = (
+  radius: number,
+  fillColor: string = "black",
+  startAngle: number = 0,
+  endAngle: number = Math.PI * 2
+) => {
+  if (ctx.value === null) return;
+  ctx.value.beginPath();
+  ctx.value.moveTo(centerCoordinate.value.x, centerCoordinate.value.y);
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    radius,
+    startAngle,
+    endAngle
+  );
+  ctx.value.fillStyle = fillColor;
+  ctx.value.fill();
+};
+
+// 惑星軌道を描画
+const drawPlanetaryOrbit = (
+  planet: Planet,
+  lineColor: string = "rgba(130,130,130,0.6)" // 軌道の色
+) => {
+  if (ctx.value === null) return;
+  ctx.value.beginPath();
+  ctx.value.strokeStyle = lineColor;
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    planet.radiusRatio * canvasScale.value / 2,
+    0,
+    2 * Math.PI
+  );
+  ctx.value.stroke();
+  ctx.value.closePath();
+};
+
+// 恒星から惑星へのライン
+const drawLineStarToPlanet = (
+  planet: Planet,
+  days: number = 0,
+  lineColor: string = "rgba(255,255,255, 0.5)", // 扇形の色
+  lineWidth: number = 1 // 扇形の色
+) => {
+  if (ctx.value === null) return;
+  ctx.value.beginPath();
+  ctx.value.lineWidth = lineWidth;
+  ctx.value.strokeStyle = lineColor;
+
+  ctx.value.moveTo(centerCoordinate.value.x, centerCoordinate.value.y);
+  ctx.value.arc(
+    centerCoordinate.value.x,
+    centerCoordinate.value.y,
+    planet.radiusRatio * canvasScale.value / 2,
+    (-1) * planet.angle(dayToYear(days)),
+    (-1) * planet.angle(dayToYear(days)),
+  )
+  ctx.value.stroke();
+  ctx.value.closePath();
+  ctx.value.lineWidth = 1; // reset
+}
+
+// 衛星軌道
+const drawMoonOrbit = (
+  moon: Moon,
+  days: number = 0,
+  lineColor: string = "rgba(99,99,99, 0.4)" // 軌道の色
+) => {
+  if (ctx.value === null) return;
+  // 惑星の位置を導出
+  const { x, y } = getXYByRadians(
+    moon.planet.angle(dayToYear(days)),
+    moon.planet.radiusRatio * canvasScale.value / 2
+  );
+  ctx.value.beginPath();
+  ctx.value.strokeStyle = lineColor;
+  ctx.value.arc(
+    centerCoordinate.value.x + x,
+    centerCoordinate.value.y + y,
+    moon.radiusRatio * canvasScale.value / 2,
+    0,
+    2 * Math.PI,
+    true
+  );
+  ctx.value.stroke();
+  ctx.value.closePath();
+};
+
+// 衛星を描画
+const drawMoon = (
+  moon: Moon,
+  days: number = 0,
+  fillColor: string = "rgba(255,255,200,0.9)"
+) => {
+  if (ctx.value === null) return;
+  // 惑星の位置を導出
+  const { x: planetX, y: planetY } = getXYByRadians(
+    moon.planet.angle(dayToYear(days)),
+    moon.planet.radiusRatio * canvasScale.value / 2
+  );
+  ctx.value.beginPath();
+  // 位置を導出
+  const { x, y } = getXYByRadians(
+    moon.angle(dayToYear(days)),
+    moon.radiusRatio * canvasScale.value / 2
+  );
+  ctx.value.arc(
+    centerCoordinate.value.x + planetX + x,
+    centerCoordinate.value.y + planetY + y,
+    2,
+    0,
+    Math.PI * 2
+  );
+
+  ctx.value.fillStyle = fillColor;
+  ctx.value.fill();
+};
+
+</script>
+
+<style scoped>
+
+</style>
+
+<template>
+  <div class="d-flex justify-center">
+    <canvas :width="canvasScale * 2" :height="canvasScale * 2" class="canvas" id="canvas">
+    </canvas>
+  </div>
+</template>
--- src/components/PlanetClock.vue
+++ src/components/PlanetClock.vue
@@ -2,77 +2,121 @@
 import { onMounted, ref, watch } from "vue";
 import SolarSystemCanvas from "./SolarSystemCanvas.vue";
 import MoonSyzygyCanvas from "./MoonSyzygyCanvas.vue";
-import { format, addDays, differenceInDays, getYear, getMonth, getDate } from "date-fns";
+import { getUnixTime, fromUnixTime, getYear, getDate, getSeconds, getMonth, getHours, getMinutes } from "date-fns";
+import { format as formatTZ } from 'date-fns-tz';
 
 defineProps({
   msg: String,
 });
 
-const mode = ref('auto_increment'); // now, auto_increment, manual
-const calendarYear = ref(1900);
-const calendarMonth = ref(2);
-const calendarDay = ref(21);
-const calendarHour = ref(0);
-const calendarMinute = ref(0);
-const calendarSecond = ref(0);
-const displayCalendar = () => {
-  return new Date(calendarYear.value, calendarMonth.value, calendarDay.value,
-    calendarHour.value, calendarMinute.value, calendarSecond.value);
-}
+const baseUnixTime = getUnixTime(new Date(1900, 2, 21, 0, 0, 0)); // 基準日時
+const unixTimeCount = ref(0); // 基準日時からの経過UnixTime
+const dayCount = () => {
+  const count = unixTimeCount.value / (60 * 60 * 24)
+  console.log(count)
+  return count
+}; // 基準日時からの経過日数
 
-const setCurrentDateTime = () => {
-  const currentDateTime = (new Date());
-  calendarYear.value = getYear(currentDateTime);
-  calendarMonth.value = getMonth(currentDateTime);
-  calendarDay.value = getDate(currentDateTime);
-}
+const countedDateTime = () => { return fromUnixTime(baseUnixTime + unixTimeCount.value) };
 
-const displayCountedDate = () => {
-  return format(countedDate(), "yyyy/MM/dd");
-}
-const displayCountedTime = () => {
-  return format(countedDate(), "hh:mm:ss");
-}
-
-const baseDate = new Date(1900, 2, 21); // 基準日
-const dayCount = ref(0); // 基準日からの経過日数
-
-const countedDate = () => { return addDays(baseDate, dayCount.value) };
-const beatSpeed = ref(40); // 1秒間のカウント数の逆数
-const speed = ref(1 / 24);
-
-const incrementHour = () => {
+// 早送り
+const beatSpeed = ref(3); // 1秒間の count 回数
+const timePerCount = ref(60 * 60 * 24); // 1カウントごとに進めるunixtime
+const incrementTime = () => {
   if (mode.value === 'auto_increment') {
-    dayCount.value += speed.value;
+    unixTimeCount.value += timePerCount.value;
   }
 }
-onMounted(() => {
-  setInterval(incrementHour, 1000 / beatSpeed.value); // 1時間(1/24日)づつカウント
 
-  // 最初の横並びの見た目が良くないため、日数を追加する
-  dayCount.value += 365.25;
+let fastForwardInterval: number; // 早送り
+let currentInterval: number; // 現在時刻
+
+
+// 現在の UNIX time を設定
+const setCurrentDateTime = () => {
+  const currentUnixTime = getUnixTime(new Date());
+  unixTimeCount.value = currentUnixTime - baseUnixTime
+}
+
+// 操作用
+const manualDateTime = ref({
+  year: 1990,
+  month: 2,
+  day: 21,
+  hour: 0,
+  minute: 0,
+  second: 0,
 });
-
-// 基準日と表示カレンダーのunixタイムを比較する
-const calendarDayFromBaseDate = () => {
-  return differenceInDays(displayCalendar(), baseDate);
+const setManualDateTime = () => {
+  const manualUnixTime = getUnixTime(new Date(
+    manualDateTime.value.year,
+    manualDateTime.value.month,
+    manualDateTime.value.day,
+    manualDateTime.value.hour,
+    manualDateTime.value.minute,
+    manualDateTime.value.second));
+  unixTimeCount.value = manualUnixTime - baseUnixTime;
+}
+const setTimeToManualDateTime = () => {
+  const dateTime = countedDateTime();
+  manualDateTime.value = {
+    year: getYear(dateTime),
+    month: getMonth(dateTime),
+    day: getDate(dateTime),
+    hour: getHours(dateTime),
+    minute: getMinutes(dateTime),
+    second: getSeconds(dateTime),
+  }
 }
 
-watch(displayCalendar, () => {
-  if (mode.value == 'manual') {
-    dayCount.value = calendarDayFromBaseDate();
-  } else if (mode.value == 'current') {
-    dayCount.value = calendarDayFromBaseDate();
-  }
-})
+// モード
+const mode = ref('current'); // current, auto_increment, manual
 
+const setCurrentMode = () => {
+  clearInterval(fastForwardInterval);
+  setCurrentDateTime();
+  currentInterval = setInterval(setCurrentDateTime, 1000); // 毎秒カウントアップ
+}
+
+const setManualMode = () => {
+  clearInterval(fastForwardInterval);
+  clearInterval(currentInterval);
+  setTimeToManualDateTime();
+}
+
+const setFastForwardMode = () => {
+  clearInterval(currentInterval);
+  fastForwardInterval = setInterval(incrementTime, 1000 / beatSpeed.value); // 1時間(1/24日)づつカウント
+}
+// モード変更検知
 watch(mode, () => {
   if (mode.value == 'manual') {
+    setManualMode()
   } else if (mode.value == 'current') {
-    setCurrentDateTime();
-    dayCount.value = calendarDayFromBaseDate();
+    setCurrentMode();
+  } else {
+    setFastForwardMode();
   }
 })
+
+// 操作時変更検知
+watch(manualDateTime, () => {
+  setManualDateTime();
+}, { deep: true })
+
+// 表示用（年月日）
+const displayCountedDate = () => {
+  return formatTZ(countedDateTime(), "yyyy/MM/dd", { timeZone: 'Asia/Tokyo' });
+}
+// 表示用（時分秒）
+const displayCountedTime = () => {
+  return formatTZ(countedDateTime(), "HH:mm:ss", { timeZone: 'Asia/Tokyo' });
+}
+
+// 初期表示の設定
+onMounted(() => {
+  setCurrentMode();
+});
 
 const model = ref(null)
 </script>
@@ -94,45 +138,61 @@ const model = ref(null)
         </v-btn-toggle>
       </v-row>
       <v-row no-gutters>
-        <v-carousel v-model="model" class="pa-4" :show-arrows="false" :hide-delimiters="true" :height="420">
+        <!-- mobile 表示の場合はカルーセル -->
+        <v-carousel v-model="model" class="pa-4" :show-arrows="false" :hide-delimiters="true" :height="450">
           <v-carousel-item>
             <v-card class="ma-4" flat>
               <div class="fill-height align-center justify-center">
-                <SolarSystemCanvas :day-count="dayCount" class="ma-2 pa-2" />
+                <SolarSystemCanvas :day-count="dayCount()" class="ma-2 pa-2" />
                 <h2 class="text-center">{{ displayCountedDate() }}</h2>
+                <h3 class="text-center">{{ displayCountedTime() }}</h3>
               </div>
             </v-card>
           </v-carousel-item>
           <v-carousel-item>
             <v-card color="grey-lighten-1" class="ma-4">
               <div class="fill-height align-center justify-center">
-                <MoonSyzygyCanvas :day-count="dayCount" class="ma-2 pa-2" />
+                <MoonSyzygyCanvas :day-count="dayCount()" class="ma-2 pa-2" />
                 <h2 class="text-center">{{ displayCountedTime() }}</h2>
               </div>
             </v-card>
           </v-carousel-item>
         </v-carousel>
+        <!-- tablet 以上の場合は横並び -->
+        <!-- <v-card class="ma-4" flat>
+          <div class="fill-height align-center justify-center">
+            <SolarSystemCanvas :day-count="dayCount()" class="ma-2 pa-2" />
+            <h2 class="text-center">{{ displayCountedDate() }}</h2>
+          </div>
+        </v-card>
+        <v-card color="grey-lighten-1" class="ma-4">
+          <div class="fill-height align-center justify-center">
+            <MoonSyzygyCanvas :day-count="dayCount()" class="ma-2 pa-2" />
+            <h2 class="text-center">{{ countedDateTime() }}</h2>
+          </div>
+        </v-card> -->
       </v-row>
     </v-card>
     <!-- 操作時 -->
     <v-card v-show="mode == 'manual'" class="pa-2">
-      <v-slider v-model="calendarYear" min="1900" max="2200" density="compact" label=" year"></v-slider>
-      <v-slider v-model="calendarMonth" min="0" max="11" density="compact" label="month"></v-slider>
-      <v-slider v-model="calendarDay" min="1" max="31" density="compact" label="  day"></v-slider>
+      <v-slider v-model="manualDateTime.year" min="1900" max="2200" density="compact" label=" year"></v-slider>
+      <v-slider v-model="manualDateTime.month" min="0" max="11" density="compact" label="month"></v-slider>
+      <v-slider v-model="manualDateTime.day" min="1" max="31" density="compact" label="  day"></v-slider>
+      <v-slider v-model="manualDateTime.hour" min="0" max="24" density="compact" label="  hour"></v-slider>
     </v-card>
     <!-- 早送り時 -->
     <v-card v-if="mode == 'auto_increment'" class="d-flex justify-center align-center">
-      <v-btn-toggle v-model="speed">
+      <v-btn-toggle v-model="timePerCount">
         <v-btn :value="0">
           <v-icon>mdi-stop</v-icon>
         </v-btn>
-        <v-btn :value="1 / 24">
+        <v-btn :value="(60 * 60)">
           <v-icon>mdi-play</v-icon>
         </v-btn>
-        <v-btn :value="6 / 24">
+        <v-btn :value="(60 * 60 * 24)">
           <v-icon>mdi-fast-forward</v-icon>
         </v-btn>
-        <v-btn :value="6">
+        <v-btn :value="(60 * 60 * 24 * 30)">
           <v-icon>mdi-skip-forward</v-icon>
         </v-btn>
       </v-btn-toggle>
